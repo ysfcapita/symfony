d3b60616dd12e2283472da0ffe301bc2c5e528c5
youssef <ysfbel7@gmail.com>
2022-12-21 15:38:26 +0100

    adding user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -13,19 +17,71 @@ class HomeController extends AbstractController
     public function index(Request $request): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
             'route_name' => $request->attributes->get('_route')
         ]);
     }
 
-    #[Route('/login', name: 'Login')]
 
-    public function login(string $_route): Response
+    // #[Route('/admin', name: 'Admin')]
+    // public function admin(string $_route): Response
+    // {
+    //     return $this->render('home/admin.html.twig', [
+    //         'route_name' => $_route
+    //     ]);
+    // }
+
+    #[Route('/product/create', name: 'product_create')]
+    #[Route('/product/update/{id}', name: 'product_update')]
+    public function addProduct(string $_route, ProductRepository $repo, Request $req, Product $product = null): Response
     {
-        return $this->render('home/login.html.twig', [
-            'controller_name' => 'LoginController',
-            'route_name' => $_route
+        // if (!$product) {
+        //     if ($_route === 'product_create') {
+        //         $product = new Product();
+        //     } else {
+        //         throw new NotFoundHttpException();
+        //     }
+        // }
+        if ($_route === 'product_update') {
+            if ($product === null) {
+                // throw new NotFoundHttpException();
+                return new Response($this->renderView('bundles/TwigBundle/Exception/error404.html.twig'), Response::HTTP_NOT_FOUND);
+            }
+        } else {
+            $product = new Product();
+        }
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->save($product, true);
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('home/product.html.twig',
+            [
+                'route_name' => $_route,
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    #[Route('/product/list', name: 'product_list')]
+    public function listProduct(string $_route, ProductRepository $repo): Response
+    {
+        $list_product = $repo->findAll();
+
+        return $this->render('home/product_list.html.twig', [
+            'route_name' => $_route,
+            'list_product' => $list_product
         ]);
     }
 
+    #[Route('/product/delete/{id}', name: 'product_delete')]
+    public function deleteProduct(string $_route, ProductRepository $repo, Product $prod): Response
+    {
+
+        $repo->remove($prod, true);
+        return $this->redirectToRoute('product_list');
+    }
 }
---
+++ src/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('category')
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success mt-3 w-100'
+                ],
+                'label' => $builder->getData()->getId() ? 'Update' : 'Create'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
